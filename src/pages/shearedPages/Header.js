4518c99eeb61f299b01ed8522590ba36d9ed12ea
Arif Bishwas <60783337+arifbiswas@users.noreply.github.com>
2022-10-25 13:17:43 +0600

    header and log page added

--- src/pages/shearedPages/Header.js
+++ src/pages/shearedPages/Header.js
@@ -1,105 +1,126 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaBookOpen, FaMoon } from 'react-icons/fa';
+import { useState } from "react";
 
 const Header = () => {
+    const [on,setOn] = useState(false);
+    const [open,setOpen]=useState(false);
+    const handleDarkMod = ()=>{
+        setOn(!on)
+    }
+
   return (
     <div>
       <div>
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
           <div className="container flex flex-wrap justify-between items-center mx-auto">
-            <a href="https://flowbite.com/" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="mr-3 h-6 sm:h-9"
-                alt="Flowbite Logo"
-              />
+            <NavLink to="/home" className="flex items-center">
+              <span className="text-2xl mr-2"><FaBookOpen></FaBookOpen></span>
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                OnL University
               </span>
-            </a>
+            </NavLink>
             <button
+            onClick={()=>setOpen(!open)}
               data-collapse-toggle="navbar-default"
               type="button"
               className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-default"
-              aria-expanded="false"
+              aria-expanded={false}
             >
               <span className="sr-only">Open main menu</span>
               <svg
                 className="w-6 h-6"
-                aria-hidden="true"
+                aria-hidden={false}
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </button>
             <div
-              className="hidden w-full md:block md:w-auto"
+              className={`${open ? '' :'hidden'} w-full md:block md:w-auto`}
               id="navbar-default"
             >
               <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <a
-                    href="#"
+                  <NavLink
+                    to="/"
                     className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </NavLink>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <NavLink
+                    to="/courses"
                     className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
-                    About
-                  </a>
+                    Courses
+                  </NavLink>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <NavLink
+                    to="/login"
                     className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
-                    Services
-                  </a>
+                    Login
+                  </NavLink>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <NavLink
+                    to="register"
                     className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
-                    Pricing
-                  </a>
+                    Register
+                  </NavLink>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <NavLink
+                    to="faq"
                     className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
-                    Contact
-                  </a>
+                    FAQ
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="blog"
+                    className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  >
+                    Blog
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="profile"
+                    className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  >
+                    User Profile
+                  </NavLink>
                 </li>
                 <li>
                   <label
-                    for="checked-toggle"
-                    class="inline-flex relative items-center cursor-pointer"
+                    htmlFor="checked-toggle"
+                    className="inline-flex relative items-center cursor-pointer"
                   >
                     <input
+                    onClick={handleDarkMod}
                       type="checkbox"
                       value=""
                       id="checked-toggle"
-                      class="sr-only peer"
-                      checked=""
+                      className="sr-only peer"
+                      checked={on}
                     />
                     <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      Dark Mod
-                    </span>
+
                   </label>
                 </li>
               </ul>
