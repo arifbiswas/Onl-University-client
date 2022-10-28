93858f64cb242addf0df31122814e775cd99a61d
Arif Bishwas <60783337+arifbiswas@users.noreply.github.com>
2022-10-28 14:34:48 +0600

    firebase enverment veriable

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,24 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId,
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export default app;
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -26,14 +5,17 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-Femn_0ymb1BSgcN8CRWZBE66ecTGW1w",
-  authDomain: "assignment-10-73c0f.firebaseapp.com",
-  projectId: "assignment-10-73c0f",
-  storageBucket: "assignment-10-73c0f.appspot.com",
-  messagingSenderId: "114328359170",
-  appId: "1:114328359170:web:cef739a355bec16b3c321b"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
+
+
